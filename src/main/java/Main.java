5bf97efda57422a7fa856dff89c83a404fdd206e
Ziaeddin Najafian <zia.najafian@gmail.com>
2025-04-11 06:15:27 +0000

    last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import io.javalin.Javalin;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*********************SATRT***********************");
+        System.out.println("*********************START***********************");
         SocialMediaController controller = new SocialMediaController();
         Javalin app = controller.startAPI();
         app.start(8080);
